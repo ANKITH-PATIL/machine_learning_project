97bbfa29b221fc0de411101c61c7d38c6bf8d35a
ankith patil <ankithpatilbusiness@gmail.com>
2022-06-15 19:57:41 +0530

    updating and checking CICD Pipeline

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    return "Starting Machine Learning Project"
+    return "Starting Machine Learning Project and checking for the second time for checking whether we have established complete CICD pipeline"
 
 if __name__=="__main__":
     app.run(debug=True)
