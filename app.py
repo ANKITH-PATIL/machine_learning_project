a8cbbe6c09079c30a42a9cdf5399c6f53b48c94b
ankith patil <ankithpatilbusiness@gmail.com>
2022-06-23 19:14:33 +0530

    updating the exception file

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 
 from flask import Flask
+import sys
+from housing.logger import logging
+from housing.exception import housing_exception
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 
 def index():
+
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing=housing_exception(e,sys)
+        logging.INFO(housing.error_message)
+        logging.INFO("We are testing logging module")
     return "Starting Machine Learning Project and checking for the second time for checking whether we have established complete CICD pipeline"
 
 if __name__=="__main__":
