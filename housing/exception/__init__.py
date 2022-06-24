07a6985e442372e98c73e97ae4d295b2eec52729
ankith patil <ankithpatilbusiness@gmail.com>
2022-06-24 10:49:57 +0530

    added changes to the exception init

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -3,7 +3,7 @@ import sys
 
 class housing_exception(Exception):
 
-    def __init__(self, error_message: Exception) -> str:
+    def __init__(self, error_message: Exception,error_detail:sys) -> str:
         super().__init__(error_message)
         self.error_message=housing_exception.get_detailed_error_message(error_message=error_message,
                                                                         error_detail=error_detail)
