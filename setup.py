5a9550b648262d958de50919b6720744900dba7a
ankith patil <ankithpatilbusiness@gmail.com>
2022-06-24 10:28:11 +0530

    updating the setup file after few rectifications

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from xml.etree.ElementTree import VERSION
 from setuptools import setup,find_packages
 from typing import List
 
+#declaring variables for setup function
 
 PROJECT_NAME="housing-predictor"
 VERSION="0.0.3"
@@ -18,12 +18,15 @@ def get_requirements_list()->List[str]:
     the name of the libraries mentioned in requirements.txt file"""
 
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_file.readlines()
+        requirement_file.readlines().remove("-e .")
+
 
 setup(
 name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
-description=PACKAGES,
+description=DESCRIPTION,
+packages=find_packages(),
 install_requires=get_requirements_list()
 )
+
